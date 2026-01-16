bb2cb269163d381c343d4308e0b83b5831febc1c
Kseniia <rydaya97@gmail.com>
2026-01-16 17:55:33 +0000

    Init Facebook Pixel and fire PageView on load

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
+
 import { LandingPage } from "../pages/LandingPage";
 
 function App() {
+  useEffect(() => {
+    ReactPixel.init("TEST_PIXEL", undefined, {
+      autoConfig: true,
+      debug: true,
+    });
+
+    ReactPixel.pageView();
+  }, []);
   return <LandingPage />;
 }
 
