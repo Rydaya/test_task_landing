bdc398b3bedc71ca3bbd03064ba63079280a9431
Kseniia <rydaya97@gmail.com>
2026-01-16 17:32:11 +0000

    Added styles

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -1,5 +1,10 @@
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = ({ children, ...props }: Props) => (
-  <button {...props}>{children}</button>
+  <button
+    {...props}
+    className="text-base text-[1.5rem] sm:text-[32px] md:text-[1.75rem] xl:text-[32px] mt-[25px] bg-[#FEE3C7] text-[#170F08] px-11 py-8 rounded-[30px]"
+  >
+    {children}
+  </button>
 );
