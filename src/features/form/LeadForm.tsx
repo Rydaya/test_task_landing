ffd320acba54989f6e12960045d3a92f2fd77f50
Kseniia <rydaya97@gmail.com>
2026-01-16 18:15:06 +0000

    Added Keratio API query and lead tracking

--- src/features/form/LeadForm.tsx
+++ src/features/form/LeadForm.tsx
@@ -1,8 +1,35 @@
+import React from "react";
+import ReactPixel from "react-facebook-pixel";
 import { Button } from "../../shared/ui/Button";
 
 export const LeadForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    ReactPixel.track("Lead");
+
+    const clickId = new URLSearchParams(window.location.search).get("click_id");
+
+    fetch("https://KEITARO_API_URL", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        click_id: clickId,
+        event: "cta_click",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Test API response:", data);
+      })
+      .catch((err) => {
+        console.log("Test API error (expected in mock):", err);
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Button type="submit">Попробовать бесплатно</Button>
     </form>
   );
